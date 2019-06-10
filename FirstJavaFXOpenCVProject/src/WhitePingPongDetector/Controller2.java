62a3c6240f0cc29b95cd364b89226e6720f6bc58
Ahad2906 <s175128@student.dtu.dk>
2019-06-10 11:03:50 +0200

    hsl - fjernet - dilate

--- FirstJavaFXOpenCVProject/src/WhitePingPongDetector/Controller2.java
+++ FirstJavaFXOpenCVProject/src/WhitePingPongDetector/Controller2.java
@@ -207,11 +207,12 @@ public class Controller2 {
                     // Dilate = elementer af størrelse (x*x)pixel (gør objekt større)
                     // Erode  = elementer af størrelse (x*x)pixel (gør objekt mindre)
                     Mat morhpOutput = new Mat();
-                    Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(4, 4));
-                    Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(10, 10));
+                    Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2));
+                    Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(4, 4));
 
                     // Forstørre elementet x gange
                     Imgproc.dilate(mask, morhpOutput, dilateElement); // 1. gang
+                    Imgproc.dilate(morhpOutput, morhpOutput, dilateElement); // 2. gang
                     Imgproc.dilate(morhpOutput, morhpOutput, dilateElement); // 2. gang
 
                     // Tegner streger/kanter af elementer i framet
